bcf3b0f3066de9e6c44da262722a1cee38791cd5
devasood <devasood@yahoo.co.in>
2015-05-27 11:40:05 +0530

    FORMS built

--- UPC/src/main/java/com/mkyong/controller/BaseController.java
+++ UPC/src/main/java/com/mkyong/controller/BaseController.java
@@ -2,12 +2,14 @@ package com.mkyong.controller;
  
 
 import java.sql.*;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
 public class BaseController {
@@ -23,7 +25,7 @@ public class BaseController {
 			Class.forName("org.postgresql.Driver");
 			Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","sandbox");
 			Statement st=con.createStatement();
-			String sql="create table if not exists product(upc varchar(14) primary key,manufacturer varchar(50),brand varchar(50),size numeric);";
+			String sql="create table if not exists product(upc varchar(14) primary key,manufacturer varchar(50),brand varchar(50),size varchar(50));";
 			st.execute(sql);
 			return true;
 			
@@ -31,15 +33,30 @@ public class BaseController {
 		return false;
 	}
 	
-	public boolean read(String name){
+	public boolean read(String upc,String manf,String brand,String size){
 		try{
 			Class.forName("org.postgresql.Driver");
 			Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","sandbox");
 			Statement st=con.createStatement();
-			String sql="select * from product where upc="+name+";";
+			if(upc.length()==0)
+			{upc="%";}
+			if(manf.length()==0)
+			{manf="%";}
+			if(brand.length()==0)
+			{brand="%";}
+			if(size.length()==0)
+			{size="%";}
+			String sql="select * from product where upc like '"+upc+"' and manufacturer like '"+manf+"' and "
+					+ "brand like '"+brand+"' and size like '"+size+"';";
 			ResultSet rs= st.executeQuery(sql);
-			rs.next();
-			obj=new Product(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
+			while(rs.next())
+			{	
+				obj.upc+="<tr><td>"+rs.getString(1)+"</td>";
+				obj.upc+="<td>"+rs.getString(2)+"</td>";
+				obj.upc+="<td>"+rs.getString(3)+"</td>";
+				obj.upc+="<td>"+rs.getString(4)+"</td>";
+				
+			}
 			return true;
 		}catch(Exception  e){logger.debug(e.toString());}
 		return false;
@@ -79,25 +96,29 @@ public class BaseController {
 		model.addAttribute("op",op);
 		model.addAttribute("glob",obj.upc);
 		
+		
 		return VIEW_INDEX;
  
 	}
  
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
+	@RequestMapping(value = "/form.asp", method = RequestMethod.GET)
+	public String welcomeName(ModelMap model,
+			@RequestParam(value="UPC") String UPC,
+			@RequestParam(value="Manufacturer") String Manf,
+			@RequestParam(value="Brand") String Brand,
+			@RequestParam(value="Size") String Size)
+	{
 		obj=new Product();
+		
 // 
 //		model.addAttribute("message", "Welcome " + name);
 //		model.addAttribute("counter", ++counter);
 //		logger.debug("[welcomeName] counter : {}", counter);
 		Boolean op=false;
 		
-		op=read(name);
+		op=read(UPC,Manf,Brand,Size);
 		model.addAttribute("op",op);
-		model.addAttribute("upc",obj.upc);
-		model.addAttribute("manf",obj.manf);
-		model.addAttribute("brand",obj.brand);
-		model.addAttribute("size",obj.size);
+		model.addAttribute("glob",obj.upc);
 		
 		return VIEW_INDEX;
  
--- UPC/target/CounterWebApp/WEB-INF/pages/index.jsp
+++ UPC/target/CounterWebApp/WEB-INF/pages/index.jsp
@@ -1,9 +1,29 @@
 <html>
 <body>
 
-<h2>Operation : ${op}</h2>
+Search by:<br>
 
+<form action="form.asp" method="get">
+<table style="width:100%">
+<tr><td>UPC:</td><td><input type="text" name="UPC" size="35"></td></tr>
+<tr><td>Manufacturer:</td><td><input type="text" name="Manufacturer" size="35"></td></tr>
+<tr><td>Brand:</td><td><input type="text" name="Brand" size="35"></td></tr>
+<tr><td>Size:</td><td><input type="text" name="Size" size="35"></td></tr>
+</table>
+<button type="submit" value="submit">Search</Button> <button type="reset" value="reset">Reset</Button>
+</form>
 
+<br>Insert into table:<br>
+
+<form action="formI.asp" method="post">
+<table style="width:100%">
+<tr><td>UPC:</td><td><input type="text" name="UPCI" size="35"></td></tr>
+<tr><td>Manufacturer:</td><td><input type="text" name="ManufacturerI" size="35"></td></tr>
+<tr><td>Brand:</td><td><input type="text" name="BrandI" size="35"></td></tr>
+<tr><td>Size:</td><td><input type="text" name="SizeI" size="35"></td></tr>
+</table>
+<button type="submit" value="submitI">Insert</Button> <button type="reset" value="resetI">Reset</Button>
+</form>
 
 <table style="width:100%" border="1" >
 <tr><th><b>UPC</th>
@@ -12,12 +32,8 @@
 <th><b>Size</th>
 </tr>
 ${glob}
-<tr><td>${upc}</td>
-<td>${manf}</td>
-<td>${brand}</td>
-<td>${size}</td>
-</tr>
-
 </table>
+
+<h2>Operation : ${op}</h2>
 </body>
 </html>
